a8c9a695c31a31c72f94dd18c8b11fe1e445a5b8
Noel Lonka <noel.lonka@gmail.com>
2024-02-07 17:43:02 +0200

    exercise 2.5 done (kurssitiedot finished(?))

--- osa2/kurssitiedot/App.jsx
+++ osa2/kurssitiedot/App.jsx
@@ -1,41 +1,4 @@
-const Header = (props) => {
-	return <h1>{props.course}</h1>
-}
-
-const Part = (props) => {
-	return <p>{props.part.name} {props.part.exercises}</p>
-}
-
-const Content = ({parts}) => {
-	return (
-		<>
-			{parts.map(part =>
-				<Part key={part.id} part={part} />
-			)}
-		</>
-	)
-}
-
-const Total = ({parts}) => {
-	
-	const result = parts.map(parts => parts.exercises).reduce((total, added) => total + added)
-
-	return (
-		<>
-			<p><b>total of {result} exercises</b></p>
-		</>
-	)
-}
-
-const Course = ({course}) => {
-	return (
-		<>
-			<Header course={course.name} />
-			<Content parts={course.parts} />
-			<Total parts={course.parts} />
-		</>
-	)
-}
+import Course from './components/Course'
 
 const App = () => {
 	const courses = [

bb113252a7ae9323cda58bf923b79f51c3960952
Noel Lonka <noel.lonka@gmail.com>
2024-02-07 16:27:07 +0200

    exercise 2.2 done (kurssitiedot step7)

--- osa2/kurssitiedot/App.jsx
+++ osa2/kurssitiedot/App.jsx
@@ -17,11 +17,23 @@ const Content = ({parts}) => {
 	)
 }
 
+const Total = ({parts}) => {
+	
+	const result = parts.map(parts => parts.exercises).reduce((total, added) => total + added)
+
+	return (
+		<>
+			<p><b>total of {result} exercises</b></p>
+		</>
+	)
+}
+
 const Course = ({course}) => {
 	return (
 		<>
 			<Header course={course.name} />
 			<Content parts={course.parts} />
+			<Total parts={course.parts} />
 		</>
 	)
 }

84807d19c43037b0cd6bbfe84bb702c97ba506f2
Noel Lonka <noel.lonka@gmail.com>
2024-02-07 16:04:57 +0200

    exercise 2.1 done (kurssitiedot step6)

---
+++ osa2/kurssitiedot/App.jsx
@@ -0,0 +1,59 @@
+
+const Header = (props) => {
+	return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+	return <p>{props.part.name} {props.part.exercises}</p>
+}
+
+const Content = ({parts}) => {
+	return (
+		<>
+			{parts.map(part =>
+				<Part key={part.id} part={part} />
+			)}
+		</>
+	)
+}
+
+const Course = ({course}) => {
+	return (
+		<>
+			<Header course={course.name} />
+			<Content parts={course.parts} />
+		</>
+	)
+}
+
+const App = () => {
+	const course = {
+		name: 'Half Stack application development',
+		id: 1,
+		parts: [
+		{
+			name: 'Fundamentals of React',
+			exercises: 10,
+			id: 1
+		},
+		{
+			name: 'Using props to pass data',
+			exercises: 7,
+			id: 2
+		},
+		{
+			name: 'State of a component',
+			exercises: 14,
+			id: 3
+		}
+		]
+	}
+
+	return (
+		<div>
+			<Course course={course} />
+		</div>
+	)
+}
+
+export default App

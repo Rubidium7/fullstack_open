6816af17697bec233134ba877e6f6796b2c2ffa6
Noel Lonka <noel.lonka@gmail.com>
2025-01-02 14:57:48 +0200

    exercise 4.7 done (apufunktioita ja yksikkötestejä step5)

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -123,6 +123,72 @@ const seven_blogs = [
 	},
 ];
 
+const eight_blogs = [
+	{
+		_id: "5a422a851b54a676234d17f7",
+		title: "React patterns",
+		author: "Michael Chan",
+		url: "https://reactpatterns.com/",
+		likes: 7,
+		__v: 0,
+	},
+	{
+		_id: "5a422aa71b54a676234d17f8",
+		title: "Go To Statement Considered Harmful",
+		author: "Edsger W. Dijkstra",
+		url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+		likes: 14,
+		__v: 0,
+	},
+	{
+		_id: "5a422b3a1b54a676234d17f9",
+		title: "Canonical string reduction",
+		author: "Edsger W. Dijkstra",
+		url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+		likes: 3,
+		__v: 0,
+	},
+	{
+		_id: "5a422b891b54a676234d17fa",
+		title: "First class tests",
+		author: "Robert C. Martin",
+		url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+		likes: 10,
+		__v: 0,
+	},
+	{
+		_id: "5a422ba71b54a676234d17fb",
+		title: "TDD harms architecture",
+		author: "Robert C. Martin",
+		url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+		likes: 0,
+		__v: 0,
+	},
+	{
+		_id: "5a422bc61b54a676234d17fc",
+		title: "Type wars",
+		author: "Robert C. Martin",
+		url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+		likes: 2,
+		__v: 0,
+	},
+	{
+		_id: "5a422bc61b54a676234d17fd",
+		title: "Finnish culture...",
+		author: "Linus Torvalds",
+		url: "https://torvalds-family.blogspot.com/2009/12/finnish-culture.html",
+		likes: 12,
+		__v: 0,
+	},
+	{
+		_id: "5a422bc61b54a676234d17fe",
+		title: "Programming",
+		author: "Linus Torvalds",
+		url: "https://torvalds-family.blogspot.com/2009/08/programming.html",
+		likes: 5,
+		__v: 0,
+	},
+];
 test("dummy returns one", () => {
 	const result = listHelper.dummy(empty_list);
 	assert.strictEqual(result, 1);
@@ -145,7 +211,7 @@ describe("total likes", () => {
 	});
 });
 
-describe("most likes", () => {
+describe("blog with most likes", () => {
 	test("of empty list is null", () => {
 		const result = listHelper.favoriteBlog(empty_list);
 		assert.strictEqual(result, null);
@@ -175,6 +241,37 @@ describe("most likes", () => {
 			likes: seven_blogs[2].likes,
 		};
 		const result = listHelper.favoriteBlog(seven_blogs);
+		assert.deepStrictEqual(result, expected_result);
+	});
+});
+
+describe("author with most likes", () => {
+	test("of empty list is null", () => {
+		const result = listHelper.mostLikes(empty_list);
+		assert.strictEqual(result, null);
+	});
+	test("when list has only one blog, returns it", () => {
+		const expected_result = {
+			author: one_blog[0].author,
+			likes: one_blog[0].likes,
+		};
+		const result = listHelper.mostLikes(one_blog);
+		assert.deepStrictEqual(result, expected_result);
+	});
+	test("of six blogs finds correct one", () => {
+		const expected_result = {
+			author: six_blogs[2].author,
+			likes: 17,
+		};
+		const result = listHelper.mostLikes(six_blogs);
+		assert.deepStrictEqual(result, expected_result);
+	});
+	test("when two has the same amount of likes, returns first one", () => {
+		const expected_result = {
+			author: eight_blogs[2].author,
+			likes: 17,
+		};
+		const result = listHelper.mostLikes(eight_blogs);
 		assert.deepStrictEqual(result, expected_result);
 	});
 });
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -14,7 +14,7 @@ const favoriteBlog = (blogs) => {
 			favorite_blog = blogs[i];
 	}
 
-	if (favorite_blog === null) return null;
+	if (!favorite_blog) return null;
 
 	return {
 		title: favorite_blog.title,
@@ -23,8 +23,35 @@ const favoriteBlog = (blogs) => {
 	};
 };
 
+const mostLikes = (blogs) => {
+	authors = {};
+	most_liked = null;
+
+	for (i = 0; i < blogs.length; i++) {
+		authors[blogs[i].author] = 0;
+	}
+
+	for (i = 0; i < blogs.length; i++) {
+		authors[blogs[i].author] += blogs[i].likes;
+	}
+
+	for (const [key, value] of Object.entries(authors)) {
+		if (!most_liked || authors[most_liked] < value) {
+			most_liked = key;
+		}
+	}
+
+	if (!most_liked) return null;
+
+	return {
+		author: most_liked,
+		likes: authors[most_liked],
+	};
+};
+
 module.exports = {
 	dummy,
 	totalLikes,
 	favoriteBlog,
+	mostLikes,
 };

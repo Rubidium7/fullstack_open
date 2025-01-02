15dbd7cd517e62b39c9d5e088c8d9c21db740d5f
Noel Lonka <noel.lonka@gmail.com>
2025-01-02 14:24:15 +0200

    exercise 4.6 done (apufunktioita ja yksikkötestejä step4)

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -6,7 +6,25 @@ const totalLikes = (blogs) => {
 	return blogs.reduce((sum, item) => sum + item.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+	favorite_blog = null;
+
+	for (i = 0; i < blogs.length; i++) {
+		if (favorite_blog === null || favorite_blog.likes < blogs[i].likes)
+			favorite_blog = blogs[i];
+	}
+
+	if (favorite_blog === null) return null;
+
+	return {
+		title: favorite_blog.title,
+		author: favorite_blog.author,
+		likes: favorite_blog.likes,
+	};
+};
+
 module.exports = {
 	dummy,
 	totalLikes,
+	favoriteBlog,
 };
